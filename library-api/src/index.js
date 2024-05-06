5f19c6099cdef905aec2e971be69f73a2e96166d
Ketly Wine <ketly.wine@gmail.com>
2024-05-06 16:49:00 -0300

    feat: modification in variables as .env

--- library-api/src/index.js
+++ library-api/src/index.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const mongoose = require('mongoose')
+require('dotenv').config();
 
 const app = express()
 app.use(express.json())
-const port = 3000
+const port = process.env.PORT || 3000
 
 const Book = mongoose.model('Book', { 
     title: String,
@@ -91,7 +92,7 @@ app.delete('/book/:id', async(req, res) => {
     }
 })
 
-mongoose.connect('mongodb+srv://ketlywine:GwrXtzgN7qpq6yXk@cluster0.h9ljeae.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 .then(() => {
     console.log('Connected to database')
     app.listen(port, () => {
